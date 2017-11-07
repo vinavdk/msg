48805a50ec8da6a88ba3ce725a611a8be8bf6bf2
vinavdk <kodingtruykich@gmail.com>
2017-11-07 16:28:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+<title>Vn-Hax.VIP</title>
+<font color="white"></font>
+<style type="text/css">
+.btn {
+ background: #3B5998;
+ color: #fff;
+ padding: 3px;
+ border: 0;
+ cursor: pointer;
+ outline: none;
+}
+.btn:hover {
+ background: #ff9900;
+}
+.btn-order {
+ background: #ff471a;
+ display: inline-block;
+}
+
+.btn-add {
+ background: #00ace6;
+ display: inline-block;
+}
+
+.btn-dat {
+ background: #ff0066;
+ display: inline-block;
+}
+</style>
+<script type="text/javascript">
+var url  = 'http://go.onclasrv.com/afu.php?id=1374220';
+
+function FacebookHandle(){
+        window.location.href = 'https://fb.com/vnhaxcf';
+}
+function DownloadHack(){
+        window.open(url,'_blank')
+        window.location.href = 'http://123link.io/NaEc';
+}
+</script>
+
+<center>
+<input class="btn btn-order" id="btndltl" onclick="DownloadHack()" style="font-size: 16px; font-weight: bold; height: 50px; width: 300px;" type="submit" value="TẢI HACK" />
+<input class="btn btn-add" id="btndldr" onclick="FacebookHandle()" style="font-size: 16px; font-weight: bold; height: 50px; width: 300px;" type="submit" value="FANPAGE FACEBOOK" />
+</center>
+</html>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-73797495-9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-73797495-9');
+</script>
